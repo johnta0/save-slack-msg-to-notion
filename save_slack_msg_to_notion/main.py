dd3fe3ff9e17ee0ef484b9cc2d8d676f0a62de1a
johnta0 <22534726+johnta0@users.noreply.github.com>
2024-08-18 19:33:54 +0900

    Update: add exclude_channel_list

--- save_slack_msg_to_notion/main.py
+++ save_slack_msg_to_notion/main.py
@@ -10,7 +10,7 @@ from notion_client.errors import APIResponseError
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -19,6 +19,7 @@ load_dotenv()
 # Notion setup
 notion = Client(auth=os.getenv("NOTION_INTEGRATION_SECRET"))
 database_id = os.getenv("NOTION_DB_ID")
+exclude_channel_list = os.getenv("EXCLUDE_CHANNEL_LIST").split(",")
 
 
 def get_existing_timestamps():
@@ -30,21 +31,25 @@ def get_existing_timestamps():
         response = notion.databases.query(
             database_id=database_id,
             start_cursor=start_cursor,
-            page_size=100  # Adjust as needed
+            page_size=100,  # Adjust as needed
         )
-        for page in response['results']:
-            timestamp = page['properties']['Timestamp']['title'][0]['text']['content']
+        for page in response["results"]:
+            timestamp = page["properties"]["Timestamp"]["title"][0]["text"]["content"]
             existing_timestamps.add(timestamp)
 
-        has_more = response['has_more']
-        start_cursor = response.get('next_cursor')
+        has_more = response["has_more"]
+        start_cursor = response.get("next_cursor")
 
-    logger.info(f"Retrieved {len(existing_timestamps)} existing timestamps from the database.")
+    logger.info(
+        f"Retrieved {len(existing_timestamps)} existing timestamps from the database."
+    )
     return existing_timestamps
 
 
 def process_slack_archive(root_dir, existing_timestamps):
     for channel_dir in os.listdir(root_dir):
+        if channel_dir in exclude_channel_list:
+            continue
         channel_path = os.path.join(root_dir, channel_dir)
         if os.path.isdir(channel_path):
             process_channel(channel_path, channel_dir, existing_timestamps)
@@ -57,20 +62,22 @@ def process_channel(channel_path, channel_name, existing_timestamps):
             with open(file_path, "r", encoding="utf-8") as file:
                 messages = json.load(file)
                 for index, message in enumerate(messages, start=1):
-                    logger.info(f"Processing message {index} of {len(messages)} in channel {channel_name}")
+                    logger.info(
+                        f"Processing message {index} of {len(messages)} in channel {channel_name}"
+                    )
                     add_to_notion(message, channel_name, existing_timestamps)
 
 
 def add_to_notion(message, channel_name, existing_timestamps):
     timestamp_float = float(message.get("ts", 0))
     timestamp_str = str(timestamp_float)
-    
+
     if timestamp_str in existing_timestamps:
         logger.info(f"Skipping duplicate message with timestamp {timestamp_str}")
         return
 
     timestamp_datetime = datetime.fromtimestamp(timestamp_float)
-    
+
     properties = {
         "Timestamp": {"title": [{"text": {"content": timestamp_str}}]},
         "Channel": {"select": {"name": channel_name}},
@@ -92,7 +99,9 @@ def add_to_notion(message, channel_name, existing_timestamps):
     }
 
     try:
-        response = notion.pages.create(parent={"database_id": database_id}, properties=properties)
+        response = notion.pages.create(
+            parent={"database_id": database_id}, properties=properties
+        )
         logger.debug(f"Notion API Response: {response}")
         existing_timestamps.add(timestamp_str)  # Add the new timestamp to the set
     except APIResponseError as e:
@@ -103,8 +112,8 @@ def add_to_notion(message, channel_name, existing_timestamps):
 def main():
     root_directory = os.getenv("SLACK_ARCHIVE_PATH")
     logger.info(f"Starting to process Slack archive in {root_directory}")
-    
+
     existing_timestamps = get_existing_timestamps()
     process_slack_archive(root_directory, existing_timestamps)
-    
+
     logger.info("Finished processing Slack archive")
